5cf1d1ff57319cc0b094265496a72b40fac82835
andre <andrlzpt@protonmail.com>
2025-02-04 19:57:36 -0300

    impl. controller receives valid multipart request body

--- src/main/java/com/wierdest/photo_data/controllers/PhotoController.java
+++ src/main/java/com/wierdest/photo_data/controllers/PhotoController.java
@@ -1,7 +1,12 @@
 package com.wierdest.photo_data.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.wierdest.photo_data.DTOs.InfoDTO;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class PhotoController {
     
    @PostMapping("/upload")
-   public ResponseEntity<String> uploadPhotos(@RequestBody String entity) {
-       return ResponseEntity.ok("Upload endpoint is working!");
+   public ResponseEntity<String> uploadPhotos(@RequestPart("file") MultipartFile file, @RequestPart("info") InfoDTO info) {       
+        return ResponseEntity.ok("Upload endpoint is working!");
    }
     
 }

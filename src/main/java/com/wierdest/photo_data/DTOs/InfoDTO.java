88d4a2c4a52dd4ec5a3e999a74ba122ca82da9b4
andre <andrlzpt@protonmail.com>
2025-02-04 19:41:13 -0300

    tdd should upload expected body + creation of infoDTO + test files

---
+++ src/main/java/com/wierdest/photo_data/DTOs/InfoDTO.java
@@ -0,0 +1,13 @@
+package com.wierdest.photo_data.DTOs;
+
+public class InfoDTO {
+   private String descriptor;
+
+    public String getDescriptor() {
+        return descriptor;
+    }
+
+    public void setDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+    } 
+}
--- src/test/java/com/wierdest/photo_data/PhotoDataApplicationTests.java
+++ src/test/java/com/wierdest/photo_data/PhotoDataApplicationTests.java
@@ -6,12 +6,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.wierdest.photo_data.DTOs.InfoDTO;
 import com.wierdest.photo_data.controllers.HelloController;
 import com.wierdest.photo_data.controllers.PhotoController;
 
@@ -60,6 +67,30 @@ class PhotoDataApplicationTests {
 
 		);
 		assertThat(response.getStatusCode().isSameCodeAs(HttpStatus.NOT_FOUND)).isFalse();
+	}
+
+	@Test
+	void photosUploadShouldReceiveCorrectBody() {
+		ClassPathResource mockFile = new ClassPathResource("test-image-png.png");
+		InfoDTO mockInfo = new InfoDTO();
+		mockInfo.setDescriptor("some descriptor string");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		body.add("file", mockFile);
+		body.add("info", mockInfo);
+
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+		ResponseEntity<String> response = this.testRestTemplate.postForEntity(
+			"http://localhost:" + port + "/photos/upload",
+			requestEntity,
+			String.class
+		);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
